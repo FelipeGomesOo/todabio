b0b40b423701546b33db5386dd35d6bc40a9fe40
Felipe Gomes <contato@felipe-gomes.com>
2024-09-26 17:03:20 -0300

    Adds clustering maps

--- components/map/PoiMarkers.tsx
+++ components/map/PoiMarkers.tsx
@@ -1,12 +1,62 @@
 "use client";
-import React, { useEffect } from "react";
-import { AdvancedMarker, Pin } from "@vis.gl/react-google-maps";
+import React, { useEffect, useState, useRef, useCallback } from "react";
+import { AdvancedMarker, Pin, useMap } from "@vis.gl/react-google-maps";
+import { MarkerClusterer } from "@googlemaps/markerclusterer";
+import type { Marker } from "@googlemaps/markerclusterer";
 
 const PoiMarkers = (props: { pois: Poi[] }) => {
+  const map = useMap();
+  const [markers, setMarkers] = useState<{ [key: string]: Marker }>({});
+  const clusterer = useRef<MarkerClusterer | null>(null);
+  const handleClick = useCallback(
+    (ev: google.maps.MapMouseEvent) => {
+      if (!map) return;
+      if (!ev.latLng) return;
+      console.log("marker clicked:", ev.latLng.toString());
+      map.panTo(ev.latLng);
+    },
+    [map]
+  );
+
+  // Initialize MarkerClusterer, if the map has changed
+  useEffect(() => {
+    if (!map) return;
+    if (!clusterer.current) {
+      clusterer.current = new MarkerClusterer({ map });
+    }
+  }, [map]);
+
+  // Update markers, if the markers array has changed
+  useEffect(() => {
+    clusterer.current?.clearMarkers();
+    clusterer.current?.addMarkers(Object.values(markers));
+  }, [markers]);
+
+  const setMarkerRef = (marker: Marker | null, key: string) => {
+    if (marker && markers[key]) return;
+    if (!marker && !markers[key]) return;
+
+    setMarkers((prev) => {
+      if (marker) {
+        return { ...prev, [key]: marker };
+      } else {
+        const newMarkers = { ...prev };
+        delete newMarkers[key];
+        return newMarkers;
+      }
+    });
+  };
+
   return (
     <>
       {props.pois.map((poi: Poi) => (
-        <AdvancedMarker key={poi.key} position={poi.location}>
+        <AdvancedMarker
+          key={poi.key}
+          position={poi.location}
+          ref={(marker) => setMarkerRef(marker, poi.key)}
+          clickable={true}
+          onClick={handleClick}
+        >
           <Pin
             background={"#FBBC04"}
             glyphColor={"#000"}
