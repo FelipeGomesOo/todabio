a9e5c1a792e146c59a82b69f1369a95ee25e8c37
Felipe Gomes <contato@felipe-gomes.com>
2024-10-05 13:03:05 -0300

    Add group charts

---
+++ components/map/GroupChart.tsx
@@ -0,0 +1,27 @@
+import { groupColors, getValuesWithNormalization } from "@/lib/utils";
+export default function GroupChart({ marker }: { marker: SampleMarker }) {
+  const data = getValuesWithNormalization(marker.DAPC);
+  return (
+    <div className="self-stretch h-72 border-t border-[#cccccc] flex-col justify-start items-start flex">
+      {Object.entries(data).map(([key, value]) => (
+        <div
+          key={key}
+          className="self-stretch py-1 border-b border-[#cccccc] justify-start items-center gap-2 inline-flex"
+        >
+          <div className="w-[7rem] self-stretch ">{key}</div>
+          <div className="grow flex gap-2 items-center">
+            <div
+              style={{ background: `${groupColors[key].color}` }}
+              className={`w-2.5 h-2.5 rounded-full shadow border border-white`}
+            ></div>
+            <div
+              style={{ width: `${value[1]}%` }}
+              className={`w-[${value[1]}%] h-2 relative bg-[#ababab] rounded-full transition-all`}
+            ></div>
+          </div>
+          <div className="w-[50px] h-5 text-right">{value[0]}%</div>
+        </div>
+      ))}
+    </div>
+  );
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -9,3 +9,89 @@ export function formatDate(dateString: string) {
   const [year, month, day] = dateString.split("-");
   return `${day}/${month}/${year}`;
 }
+
+export function getMaxGroup(markerData: { [key: string]: number }) {
+  return Object.keys(markerData).reduce((maxGroup, currentGroup) => {
+    return markerData[currentGroup] > markerData[maxGroup]
+      ? currentGroup
+      : maxGroup;
+  });
+}
+
+export const groupColors: {
+  [key: string]: { bg: string; border: string; color: string };
+} = {
+  Acre: {
+    color: "#BBFF00",
+    bg: "bg-[#BBFF00]",
+    border: "border-chart-lightGreen",
+  },
+  Jupiá: {
+    color: "#5FB456",
+    bg: "bg-[#5FB456]",
+    border: "border-chart-darkGreen",
+  },
+  Rosana: { color: "#00F3EB", bg: "bg-[#00F3EB]", border: "border-chart-cyan" },
+  "Três Irmãos": {
+    color: "#3355FF",
+    bg: "bg-[#3355FF]",
+    border: "border-chart-blue",
+  },
+  Guadalupe: {
+    color: "#DA21FF",
+    bg: "bg-[#DA21FF]",
+    border: "border-chart-purple",
+  },
+  "Bom Jardim": {
+    color: "#FF00AA",
+    bg: "bg-[#FF00AA]",
+    border: "border-chart-pink",
+  },
+  Florença: {
+    color: "#FF0000",
+    bg: "bg-[#FF0000]",
+    border: "border-chart-red",
+  },
+  Madureira: {
+    color: "#FF7300",
+    bg: "bg-[#FF7300]",
+    border: "border-chart-orange",
+  },
+  Pirajá: {
+    color: "#FFE800",
+    bg: "bg-[#FFE800]",
+    border: "border-chart-yellow",
+  },
+};
+
+export const currentMarker = "COI";
+
+type DAPCData = {
+  [key: string]: number; // Define um tipo para os dados de entrada
+};
+
+type NormalizedValues = {
+  [key: string]: [number, number]; // Define um tipo para os valores normalizados
+};
+export const getValuesWithNormalization = (
+  data: DAPCData
+): NormalizedValues => {
+  const maxValue = Math.max(...Object.values(data)); // Encontra o valor máximo
+  const result: NormalizedValues = {}; // Cria um objeto para armazenar os resultados
+
+  for (const key in data) {
+    const normalizedValue = ((data[key] / maxValue) * 100).toFixed(2); // Normaliza
+    result[key] = [data[key], parseFloat(normalizedValue)]; // Adiciona o valor e o valor normalizado
+  }
+
+  // Ordena os resultados do maior para o menor com base no valor normalizado
+  const sortedResult = Object.entries(result).sort(
+    ([, a], [, b]) => b[1] - a[1]
+  );
+
+  // Converte de volta para um objeto
+  const orderedNormalizedValues: NormalizedValues =
+    Object.fromEntries(sortedResult);
+
+  return orderedNormalizedValues; // Retorna o objeto resultante ordenado
+};
